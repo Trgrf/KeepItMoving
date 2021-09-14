e6e6489403d555f363a451a12aecc627dfc649b6
Jemel Williams <jwilliams25400@yahoo.com>
2021-09-14 11:44:04 -0400

    started models exercise

---
+++ models/Exercise.js
@@ -0,0 +1,10 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connections");
+
+// create a workout model
+class Exercise extends Model {}
+
+Exercise.init({
+
+    
+})
