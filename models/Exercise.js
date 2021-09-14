5f9fe1847a6c65ab8445da820f0eebcb0fd426d9
Jemel Williams <jwilliams25400@yahoo.com>
2021-09-14 11:53:30 -0400

    completed exercise models

--- models/Exercise.js
+++ models/Exercise.js
@@ -4,7 +4,27 @@ const sequelize = require("../config/connections");
 // create a workout model
 class Exercise extends Model {}
 
-Exercise.init({
+Exercise.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
 
-    
-})
+  {
+    sequelize,
+    timestamp: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "exercise",
+  }
+);
+
+module.exports = Exercise;
