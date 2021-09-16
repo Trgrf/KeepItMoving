83db411249283ec2541818488544241ea2b9fb36
Theo <trgrf1@gmail.com>
2021-09-16 12:02:12 -0400

    saving the loginFormSubmission

--- public/script/login.js
+++ public/script/login.js
@@ -5,6 +5,16 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
 
     if (email && password) {
-        
+        const response = await fetch('/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password}),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
     }
-}
+};
