82424856d2c757f582619c3428501834c0d523e2
Theo <trgrf1@gmail.com>
2021-09-16 10:30:59 -0400

    saving the workout delete method

--- routes/api/workoutRoutes.js
+++ routes/api/workoutRoutes.js
@@ -13,6 +13,26 @@ router.post('/', withAuth, async (req, res) => {
     } catch (err) {
         res.status(400).json(err);
     }
-})
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const workoutData = await Workout.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!workoutData) {
+            res.status(400).json({ message: 'No workout found with this id!' });
+            return;
+        }
+
+        res.status(200).json(workoutData);
+    } catch (err) { 
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
