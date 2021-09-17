47938c9c8931df98e5b500f2266e7a4b1b59d1ef
Theo <trgrf1@gmail.com>
2021-09-17 11:59:14 -0400

    saving get method

--- public/script/profile.js
+++ public/script/profile.js
@@ -2,13 +2,33 @@ const newFormHandler = async (event) => {
     event.preventDefault();
 
     const name = document.querySelector('#exercise').value.trim();
+    const weight = document.querySelector('#amt-weight').value.trim();
     const sets = document.querySelector('#sets').value.trim();
     const reps = document.querySelector('#reps').value.trim();
+    const minutes = document.querySelector('#minutes').value.trim();
+    const distance = document.querySelector('#distance').value.trim();
 
-    if (name && date && length) {
+    if (name) {
+      const response = await fetch('/api/exercise', {
+          method: 'GET',
+          
+          headers: {
+              'Content-Type': 'applicaion/json',
+          },
+      });
+
+      if (response.ok) {
+          document.location.replace('/profile');
+      } else {
+          alert('Failed to find exercise');
+      }
+  }
+
+
+    if (name && weight && sets && reps && minutes) {
         const response = await fetch('/api/exercise', {
             method: 'POST',
-            body: JSON.stringify({ name, sets, reps }),
+            body: JSON.stringify({ name, weight, sets, reps, minutes, distance }),
             headers: {
                 'Content-Type': 'applicaion/json',
             },
