9aab401ccee391e040ffa9d200246ce1fd94acfa
Theo <trgrf1@gmail.com>
2021-09-17 12:07:20 -0400

    saving the js get fetch

--- public/script/profile.js
+++ public/script/profile.js
@@ -1,61 +1,62 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const name = document.querySelector('#exercise').value.trim();
-    const weight = document.querySelector('#amt-weight').value.trim();
-    const sets = document.querySelector('#sets').value.trim();
-    const reps = document.querySelector('#reps').value.trim();
-    const minutes = document.querySelector('#minutes').value.trim();
-    const distance = document.querySelector('#distance').value.trim();
+  const name = document.querySelector('#exercise').value.trim();
+  const weight = document.querySelector('#amt-weight').value.trim();
+  const sets = document.querySelector('#sets').value.trim();
+  const reps = document.querySelector('#reps').value.trim();
+  const minutes = document.querySelector('#minutes').value.trim();
+  const distance = document.querySelector('#distance').value.trim();
 
-    if (name) {
-      const response = await fetch('/api/exercise', {
-          method: 'GET',
-          
-          headers: {
-              'Content-Type': 'applicaion/json',
-          },
-      });
-
-      if (response.ok) {
-          document.location.replace('/profile');
-      } else {
-          alert('Failed to find exercise');
-      }
-  }
-
-
-    if (name && weight && sets && reps && minutes) {
-        const response = await fetch('/api/exercise', {
-            method: 'POST',
-            body: JSON.stringify({ name, weight, sets, reps, minutes, distance }),
-            headers: {
-                'Content-Type': 'applicaion/json',
-            },
-        });
-
-        if (response.ok) {
-            document.location.replace('/profile');
-        } else {
-            alert('Failed to create exercise');
-        }
+  if (name) {
+    const response = await fetch('/api/exercise', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'applicaion/json',
+      },
+    });
+    const json = await result.json();
+    
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to find exercise');
     }
+    return json;
+}
+
+
+if (name && weight && sets && reps && minutes) {
+  const response = await fetch('/api/exercise', {
+    method: 'POST',
+    body: JSON.stringify({ name, weight, sets, reps, minutes, distance }),
+    headers: {
+      'Content-Type': 'applicaion/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace('/profile');
+  } else {
+    alert('Failed to create exercise');
+  }
+}
 };
 
 const delBtnHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-        const id = event.target.getAttribute('data-id');
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
 
-        const response = await fetch(`/api/exercise/${id}`, {
-            method: 'DELETE',
-          });
+    const response = await fetch(`/api/exercise/${id}`, {
+      method: 'DELETE',
+    });
 
-          if (response.ok) {
-            document.location.replace('/profile');
-          } else {
-            alert('Failed to delete exercise');
-          }
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete exercise');
     }
+  }
 };
 
 document
